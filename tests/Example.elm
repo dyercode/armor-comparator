c7b2e4d880ac51b184db6a819773e93482aee4d6
Jon <dyercode@users.noreply.github.com>
2020-09-18 15:45:42 -0400

    fuzzificate.

--- tests/Example.elm
+++ tests/Example.elm
@@ -2,28 +2,37 @@ module Example exposing (..)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
+import Main exposing (Armor, EnchantedArmor, Modifications, defaultModifications, plusify, totalMaxDex)
 import Test exposing (..)
-import Main exposing (EnchantedArmor, Armor, Modifications, plusify, defaultModifications, totalMaxDex )
 
 
 plusSuite =
-    describe "plussify" 
+    describe "plussify"
         [ test "Prepends the plus sign to a number" <|
-                \() -> plusify 1 |> Expect.equal "+1"
+            \() -> plusify 1 |> Expect.equal "+1"
         , test "Except when the number is negative" <|
             \() -> plusify -1 |> Expect.equal "-1"
         ]
 
-tmdSuite =
-    let 
-      someArmor = { name = "Full plate",
-         armor = 9, maxDex = 1, checkPenalty = -6, cost = 1500 
-        }
-    in 
-    describe "totalMaxDex"
-        [ test "is the raw value when not mithral" <| 
-            \() -> totalMaxDex (Main.EnchantedArmor someArmor defaultModifications "1") |> Expect.equal someArmor.maxDex
 
-        , test "is inclreased by 2 for mithral" <|
-          \() -> (totalMaxDex (Main.EnchantedArmor someArmor ({defaultModifications |  mithral= True })) "1") |> Expect.equal (someArmor.maxDex + 2)
+tmdSuite =
+    let
+        someArmor : Armor
+        someArmor =
+            { name = "Full plate"
+            , armor = 9
+            , maxDex = 1
+            , checkPenalty = -6
+            , cost = 1500
+            }
+    in
+    describe "totalMaxDex"
+        [ fuzz int "is the raw value when not mithral" <|
+            \num ->
+                totalMaxDex (Main.EnchantedArmor { someArmor | maxDex = num } defaultModifications "2")
+                    |> Expect.equal num
+        , fuzz int "is inclreased by 2 for mithral" <|
+            \num ->
+                totalMaxDex (Main.EnchantedArmor { someArmor | maxDex = num } { defaultModifications | mithral = True } "1")
+                    |> Expect.equal (num + 2)
         ]
