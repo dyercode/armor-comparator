37b65daac96a04c4e519d22ab7bba83298e6517d
Jon D <dyercode@users.noreply.github.com>
2020-09-23 20:42:13 -0400

    haven't been using this for ages.

--- app/static/data/armor.js
+++
@@ -1,5 +0,0 @@
-export const armorData = [
-	{"name":"Full plate","armor":9,"maxDex":1,"checkPenalty":-6,"cost":1500,"type":"heavy"},
-	{"name":"Half-plate","armor":8,"maxDex":0,"checkPenalty":-7,"cost":600,"type":"heavy"},
-	{"name":"Banded mail","armor":7,"maxDex":1,"checkPenalty":-6,"cost":250,"type":"heavy"}
-]
