72e9536a4b25a6d6285d5b88530ad29af07c86b2
Jon <dyercode@users.noreply.github.com>
2023-02-24 17:27:48 -0500

    container shorthand.

--- flake.nix
+++ flake.nix
@@ -3,17 +3,18 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    dev.url =
-      "github:dyercode/dev?rev=ffbbd50d25307bcbd3512996ba1a8db0e8b4d385";
+    dev.url = "github:dyercode/dev?rev=ffbbd50d25307bcbd3512996ba1a8db0e8b4d385";
+    container.url = "github:dyercode/cnt";
   };
 
-  outputs = { self, nixpkgs, flake-utils, dev }:
+  outputs = { self, nixpkgs, flake-utils, dev, container }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             dev.defaultPackage.${system}
+            container.defaultPackage.${system}
             yarn
             buildah
             fish
@@ -22,6 +23,7 @@
           shellHook = ''
             export RUNNER="podman"
             export BUILDER="buildah"
+            export IMAGE="armor"
           '';
         };
       });
